ab9e65b4b4f0e4b9d036cb08446291bfcb10a751
Andrew <dstanto2@mailfence.com>
2021-08-11 11:30:57 -0700

    fix(linreg): fix file path

--- linreg.py
+++ linreg.py
@@ -1,28 +1,45 @@
+
+
+import os
 import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
-
+import calcCost as c
+import gradientDescent as gd
 
 
 ##### load data #####
-data = pd.read_csv('/home/dstanto2/Documents/GettingAJob/Online Courses/Linear Regression Assignment/ex1data1.txt')
+data = pd.read_csv(os.getcwd() + '/ex1data1.txt')
 data.columns = ['X', 'y']
-
-X = torch.cat(torch.ones(data.shape[0]), torch.tensor(data['X'].values))
+# print(data)
+X = torch.tensor([np.array(torch.ones(data.shape[0])), np.array(torch.tensor(data['X'].values))]).transpose(0, 1)
 y = torch.tensor(data['y'].values)
 
 ##### set parameters #####
-theta = torch.tensor([0, 0])
-iterations = 1500
-alpha = 0.01
-print(theta)
+# theta = torch.squeeze(torch.tensor(torch.zeros(2, 1), dtype=torch.float64))
+theta = torch.tensor([0, 0], dtype=torch.float64)
+
+# print(theta.size())
+
+# print(X.dtype)
+# print(y.dtype)
+# print(theta.dtype)
+
+
 
 ##### compute cost #####
+cost = c.calcCost(X, y, theta)
+print("cost", cost)
 
 ##### gradient descent #####
+iters = 1500
+alpha = 0.01
+optimizedTheta = gd.gradientDescent(X, y, theta, alpha, iters)
+print("optimized theta", optimizedTheta)
+print("optimized cost", c.calcCost(X, y, optimizedTheta))
 
 ##### compute optimized cost #####
 
