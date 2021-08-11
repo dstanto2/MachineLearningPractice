7d488eeef14d258c77b12ff3fa822d6b4968ca82
Andrew <dstanto2@mailfence.com>
2021-08-11 11:38:40 -0700

    fix(linreg): fix issues

--- linreg.py
+++ linreg.py
@@ -1,6 +1,5 @@
-
-
 import os
+import sys
 import numpy as np
 import pandas as pd
 import torch
@@ -10,40 +9,40 @@ import matplotlib.pyplot as plt
 import calcCost as c
 import gradientDescent as gd
 
+if __name__ == '__main__':
 
-##### load data #####
-data = pd.read_csv(os.getcwd() + '/ex1data1.txt')
-data.columns = ['X', 'y']
-# print(data)
-X = torch.tensor([np.array(torch.ones(data.shape[0])), np.array(torch.tensor(data['X'].values))]).transpose(0, 1)
-y = torch.tensor(data['y'].values)
+    ##### load data #####
+    data = pd.read_csv(os.getcwd() + '/' + sys.argv[1])
+    data.columns = ['X', 'y']
+    # print(data)
+    X = torch.tensor([np.array(torch.ones(data.shape[0])), np.array(torch.tensor(data['X'].values))]).transpose(0, 1)
+    y = torch.tensor(data['y'].values)
+    print("X", X)
 
-##### set parameters #####
-# theta = torch.squeeze(torch.tensor(torch.zeros(2, 1), dtype=torch.float64))
-theta = torch.tensor([0, 0], dtype=torch.float64)
+    ##### set parameters #####
+    # theta = torch.squeeze(torch.tensor(torch.zeros(2, 1), dtype=torch.float64))
+    theta = torch.tensor([0, 0], dtype=torch.float64)
 
-# print(theta.size())
+    # print(theta.size())
 
-# print(X.dtype)
-# print(y.dtype)
-# print(theta.dtype)
+    # print(X.dtype)
+    # print(y.dtype)
+    # print(theta.dtype)
 
+    ##### compute cost #####
+    cost = c.calcCost(X, y, theta)
+    print("cost", cost)
 
+    ##### gradient descent #####
+    iters = 1500
+    alpha = 0.01
 
-##### compute cost #####
-cost = c.calcCost(X, y, theta)
-print("cost", cost)
+    ##### compute optimized cost #####
+    optimizedTheta = gd.gradientDescent(X, y, theta, alpha, iters)
+    print("optimized theta", optimizedTheta)
+    print("optimized cost", c.calcCost(X, y, optimizedTheta))
 
-##### gradient descent #####
-iters = 1500
-alpha = 0.01
-optimizedTheta = gd.gradientDescent(X, y, theta, alpha, iters)
-print("optimized theta", optimizedTheta)
-print("optimized cost", c.calcCost(X, y, optimizedTheta))
-
-##### compute optimized cost #####
-
-##### visualize data and predictions #####
+    ##### visualize data and predictions #####
 
 
 
