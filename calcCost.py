34b3e3c6da1b5c1b8e1b7e9b88fb63582d74a8ea
Andrew <dstanto2@mailfence.com>
2021-08-11 11:01:59 -0700

    dev(linreg): complete simple linear regression with gradient descent

--- calcCost.py
+++ calcCost.py
@@ -1,8 +1,18 @@
+import torch
+
+
 def calcCost(X, y, theta):
     pass
     # get length of X/y
+    m = y.size(0)
     # compute yhat
+    yhat = torch.matmul(X, theta)
     # compute ydiff
+    ydiff = yhat - y
     # compute ydiffsquared
+    ydiffsquared = torch.pow(ydiff, 2.0)
     # compute sumdiffsquared
+    sumdiffsquared = torch.sum(ydiffsquared)
     #return cost
+    cost = sumdiffsquared / (m * 2.0)
+    return cost
--- gradientDescent.py
+++ gradientDescent.py
@@ -1,8 +1,18 @@
-def gradientDescent(X, y, theta, alpha, num_iters):
-    pass
+import torch
+import calcCost
+
+
+def gradientDescent(X, y, theta, alpha, iters):
     # get length of X/y
-    # for each iteration:
-        # compute yhat
-        # compute ydiff
-        # compute ydiffx
+    m = y.size(0)
+    for i in range(iters):
+        yhat = torch.matmul(X, theta)
+        ydiff = yhat - y
+        ydiffx = ydiff * X[:, 1]
+        # print("yhat", yhat)
+        # print("ydiff", ydiff)
+        # print("ydiffx", ydiffx)
+
         # compute new theta
+        theta = torch.tensor([theta[0] - (alpha / m * torch.sum(ydiff)), theta[1] - (alpha / m * torch.sum(ydiffx))], dtype=torch.float64)
+    return theta
